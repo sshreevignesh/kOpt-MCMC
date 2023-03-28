ccb9f90df25037f8152115879f63fa1f55214194
Shreevignesh <sshreevignesh@gmail.com>
2023-03-28 12:10:00 +0530

    Updated Readme

--- Code/edgepollutionfileparser.py
+++ Code/edgepollutionfileparser.py
@@ -5,10 +5,6 @@ import sumolib
 sys.path.insert(1, '../Maps/Berkeley')
 import sumoMCMCconfig as MCMCconfig
 
-# def getnumsol(maxflow,vehperhour):
-#     tree = ET.parse('../newnewHyderadab_demo/osm.passenger.trips.xml')
-#     v = tree.getroot().findall('vehicle')
-#     return len(v)/(maxflow*vehperhour)
 
 def parseXML(xmlfile,net,numsol):
     
@@ -27,14 +23,14 @@ def parseXML(xmlfile,net,numsol):
         edgewisepoll[i.get('id')]=float(i.get('NOx_abs'))/numsol
 
         
-    print(sum(nox)/len(nox),end = ",")
-    print(max(nox), end = ",")
-    print(sum(nox), end = ",")
-    print()
-    # print("Mean Pollution: ",sum(nox)/len(nox))
-    # print("Max Pollution: ",max(nox))
-    # print("Total Pollution: ",sum(nox))
+    # print(sum(nox)/len(nox),end = ",")
+    # print(max(nox), end = ",")
+    # print(sum(nox), end = ",")
     # print()
+    print("Mean Pollution: ",sum(nox)/len(nox))
+    print("Max Pollution: ",max(nox))
+    print("Total Pollution: ",sum(nox))
+    print()
     
     plot_nox = {}
     plotline_nox = {}
@@ -57,14 +53,6 @@ def parseXML(xmlfile,net,numsol):
         plotline_nox[id]=nox[iter]
         
     
-    # with open('nox_heatmap.csv', mode='w') as pollution_file:
-    #     pollution_writer = csv.writer(
-    #         pollution_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #     pollution_writer.writerow(["Longitude", "Latitude", "Value"])
-    #     for i in plot_nox:
-    #         lon, lat = net.convertXY2LonLat(i[0], i[1])
-    #         pollution_writer.writerow([lon, lat, plot_nox[i]])
-    #     pollution_file.close()
     
     with open('nox_heatline.csv', mode='w') as pollution_file:
         pollution_writer = csv.writer(
@@ -77,49 +65,6 @@ def parseXML(xmlfile,net,numsol):
             tolon,tolat = net.convertXY2LonLat(p2[0], p2[1])
             pollution_writer.writerow([fromlat,fromlon,tolat,tolon,plotline_nox[i]])
         pollution_file.close()
-    
-    # with open('face_heatmap.csv', mode = 'w') as pollution_file:
-    #     pollution_writer = csv.writer(
-    #         pollution_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #     pollution_writer.writerow(["ID","Poll","geometry"])
-    #     edgefacemap = {}
-    #     for i in range(len(MCMCconfig.faces)):
-    #         face = MCMCconfig.faces[i]
-    #         for edge in face:
-    #             if edge in MCMCconfig.edgesID:    
-    #                 id = MCMCconfig.edgesID[edge]
-    #                 if id in edgefacemap:
-    #                     edgefacemap[id].append(i)
-    #                 else:
-    #                     edgefacemap[id]=[i]
-    #             if (edge[1],edge[0]) in MCMCconfig.edgesID:
-    #                 id = MCMCconfig.edgesID[(edge[1],edge[0])]
-    #                 if id in edgefacemap:
-    #                     edgefacemap[id].append(i)
-    #                 else:
-    #                     edgefacemap[id]=[i]
-    #     facepoll = [0 for i in range(len(MCMCconfig.faces))]
-    #     for i in plotline_nox:
-    #         if plotline_nox[i]!=0:
-    #             for j in edgefacemap[i]:
-    #                 facepoll[j]+=plotline_nox[i]
-        
-    #     for i in range(len(facepoll)):
-    #         facepoll[i] = facepoll[i]/len(MCMCconfig.faces[i])
-    #         coords = []
-    #         coords.append(net.getNode(MCMCconfig.faces[i][0][0]).getCoord())
-    #         for edge in MCMCconfig.faces[i]:
-    #             coords.append(net.getNode(edge[1]).getCoord())
-    #         polygon = "POLYGON (("
-    #         for j in coords:
-    #             lon, lat = net.convertXY2LonLat(j[0],j[1])
-    #             polygon = polygon + (str(lon))+ " "+str(lat)+", "
-    #         polygon = polygon[:len(polygon)-2] + "))"
-    #         pollution_writer.writerow([i,facepoll[i],polygon])
-        
-    #     # print(max(facepoll))
-    
-    #     pollution_file.close()
     
 net = sumolib.net.readNet('../Maps/Berkeley/berkeley.net.xml')
 parseXML(sys.argv[1],net,int(sys.argv[2]))
--- README.md
+++ README.md
@@ -23,3 +23,33 @@ here, `runnumber` is an identifer we used to prevent the overwriting of the outp
 `num_iter` is the number of iterations we wnat our algorithm to run for.
 
 `lambda * 100` is the value of lambda multipled by 100.if lambda is 0.5 then the value will be 50, if lambda is 0.01 then it will be 1 etc.
+
+The above code will generate the paths and save it in the following folder
+
+`filepath + str(int(alpha*100))+"A" + str(num_iter) + "I" + str(int(lamda*100)) + "l"`
+
+Here `filepath` is a prefix you can modify in the `kOptMCMC.py` file
+
+Once we have the output, we need to convert it to `passengers.trips.xml` file for SUMO and we need to run the following command for that 
+```
+cp <kOptMCMC output file> ../Maps/<Mapname>/osm.passenger.trips.xml
+python3 generatepassengertrips.py ../Maps/<Mapname> <MaxFlow Value>
+```
+
+This will generate the passenger.trips file which can be used to start the SUMO simulations. The command for the same is as follows 
+```
+sumo -c ..\Maps\<Map name>\osm.sumocfg --additional-files additonalfile.add.xml
+```
+
+This will run the SUMO simulations and will generate the pollution output. The output can be parsed using the following command (Do note that you have to change the paths within the code file for the corresponding Maps)
+
+```
+python3 edgepollutionfileparser.py additionaloutput 7
+```
+
+This will print the Mean,Max and Total pollution for the simulation. In order to generate the Heatmaps, we can run the following command 
+
+```
+python3 generateHeatlines.py nox_heatline.py <MapName>
+```
+The above command will create the heatmap and save it as an HTML file
